e2a5b37e51eb72f64df24dd757af59c96d8bf068
Nestor Ventura <nestorventura08@gmail.com>
2023-03-19 12:44:14 -0600

    inicializar proyecto

---
+++ BackEndApi/BackEndApi/Services/Implementacion/DepartamentoService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BackEndApi.Models;
+using BackEndApi.Services.Contrato;
+using System.ComponentModel;
+
+namespace BackEndApi.Services.Implementacion
+{
+    public class DepartamentoService : IDepartamentoService
+    {
+        private DbempleadoContext _dbContext;
+
+        public DepartamentoService(DbempleadoContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Departamento>> GetList()
+        {
+            try
+            {
+                List<Departamento> lista = new List<Departamento>();
+                lista = await _dbContext.Departamentos.ToListAsync();
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
