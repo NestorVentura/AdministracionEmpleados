4f628f06578fb8a1d8cc919a80b85460fd378f4d
Nestor Ventura <nestorventura08@gmail.com>
2023-03-25 11:18:46 -0600

    proyecto de angular

---
+++ FronEndCrud/src/app/Services/empleado.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+
+import {HttpClient} from '@angular/common/http';
+import {environment} from 'src/environments/environment';
+import {Observable} from 'rxjs';
+import { Empleado } from '../interfaces/empleado';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmpleadoService {
+
+  private endpoint:string = environment.endPoint;
+  private apirUrl:string = this.endpoint + "empleado/";
+
+
+  constructor(private http:HttpClient) { }
+
+  getList():Observable<Empleado[]>{
+    return this.http.get<Empleado[]>(`${this.apirUrl}lista`);
+
+}
+
+add(modelo:Empleado): Observable<Empleado>{
+  return this.http.post<Empleado>(`${this.apirUrl}guardar`, modelo);
+}
+
+update(idEmpleado:number,modelo:Empleado): Observable<Empleado>{
+  return this.http.put<Empleado>(`${this.apirUrl}actualizar/${idEmpleado}`, modelo);
+}
+
+delete(idEmpleado:number): Observable<void>{
+  return this.http.delete<void>(`${this.apirUrl}eliminar/${idEmpleado}`);
+}
+}
